a085fedd1f99ae515dd55211071ba19dacf075f6
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2023-11-05 16:15:18 +0100

    Add ssm debugging and google worker.

--- ssm_postgresql_to_gsheets.py
+++ ssm_postgresql_to_gsheets.py
@@ -14,51 +14,61 @@ ssm_document_name = os.environ.get("SSM_DOCUMENT_NAME")
 host = os.environ.get("HOST")
 
 ssm_parameters = {
-    "host": host,
-    "portNumber": "5432",
-    "localPortNumber": "5432",
+    "host": [host],
+    "portNumber": ["5432"],
+    "localPortNumber": ["5432"],
 }
 
 # PostgreSQL credentials
 db_type = "PostgreSQL"
 db_host = "localhost"
-db_port = 5432
+db_port = "5432"
 
 db_user = os.environ.get("DB_USER")
 db_password = os.environ.get("DB_PASSWORD")
-db_name = os.environ.get("PRODUCTION")
+db_name = os.environ.get("DB_NAME")
 
 # Google Sheets credentials (replace with your own)
-gspread_credentials_file = "your-credentials.json"
-spreadsheet_name = "Your Google Sheet Name"
+gspread_credentials_file = os.environ.get("GOOGLE_SHEETS_KEY_FILE")
+spreadsheet_name = os.environ.get("GOOGLE_SHEETS_FILE_NAME")
 
 # Establish an SSM session
 ssm = boto3.client("ssm", region_name=region)
 ssm_start_response = ssm.start_session(
     Target=instance_id, DocumentName=ssm_document_name, Parameters=ssm_parameters
 )
+# Print a message when the session is successfully made
+if ssm_start_response.get("SessionId"):
+    print("SSM session successfully established.")
 
-# PostgreSQL connection
-conn = psycopg2.connect(
-    dbname=db_name,
-    user=db_user,
-    password=db_password,
-    host=db_host,
-    port=db_port,
-)
+    print([
+        db_name,
+        db_user,
+        db_password,
+        db_host,
+        ssm_parameters["localPortNumber"][0],
+    ])
+    # PostgreSQL connection
+    conn = psycopg2.connect(
+        dbname=db_name,
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=ssm_parameters["localPortNumber"][0],
+    )
 
-# Download data from the PostgreSQL table as a CSV
-query = "SELECT * FROM your_table_name"
-data = pd.read_sql_query(query, conn)
+    # Download data from the PostgreSQL table as a CSV
+    query = "SELECT * FROM account_transactions"
+    data = pd.read_sql_query(query, conn)
 
-# Close the PostgreSQL connection
-conn.close()
+    # Close the PostgreSQL connection
+    conn.close()
 
-# Upload data to Google Sheets
-gc = gspread.service_account(filename=gspread_credentials_file)
-spreadsheet = gc.open(spreadsheet_name)
-worksheet = spreadsheet.get_worksheet(0)  # Adjust the worksheet index as needed
-worksheet.update([data.columns.values.tolist()] + data.values.tolist())
+    # Upload data to Google Sheets
+    gc = gspread.service_account(filename=gspread_credentials_file)
+    spreadsheet = gc.open(spreadsheet_name)
+    worksheet = spreadsheet.get_worksheet(0)  # Adjust the worksheet index as needed
+    worksheet.update([data.columns.values.tolist()] + data.values.tolist())
 
-# End the SSM session
-ssm.end_session(SessionId=ssm_start_response["SessionId"])
+    # End the SSM session
+    ssm.end_session(SessionId=ssm_start_response["SessionId"])
