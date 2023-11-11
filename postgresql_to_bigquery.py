c0192109cd67cee9f4ad86a3e94d0f7bad816e06
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2023-11-11 20:19:02 +0100

    Finish postgres to bigquery etl script.

--- postgresql_to_bigquery.py
+++ postgresql_to_bigquery.py
@@ -6,11 +6,6 @@ from google.cloud import bigquery
 
 load_dotenv()
 
-# BigQuery credentials (replace with your own)
-project_id = os.environ.get("BIGQUERY_PROJECT_ID")
-dataset_id = os.environ.get("BIGQUERY_DATASET_ID")
-table_id = os.environ.get("BIGQUERY_TABLE_ID")
-
 # PostgreSQL credentials
 db_type = "PostgreSQL"
 db_host = "localhost"
@@ -37,19 +32,27 @@ data['updated_at'] = data['updated_at'].astype(str)
 data['completed_at'] = data['completed_at'].astype(str)
 data['amount_minor_units'] = (data['amount_minor_units']).fillna(0).astype(int) / 100
 
+
+# # BigQuery credentials (replace with your own)
+bigquery_project = os.environ.get("BIGQUERY_PROJECT_ID")
+dataset_id = os.environ.get("BIGQUERY_DATASET_ID")
+table_id = os.environ.get("BIGQUERY_TABLE_ID")
+gcp_credentials = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+
+# Set the environment variable for credentials
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gcp_credentials
+
 # Initialize BigQuery client
-client = bigquery.Client(project=project_id)
+client = bigquery.Client(project=bigquery_project)
 
 # Push data to BigQuery table
 job_config = bigquery.LoadJobConfig(
     write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,  # Replace with the desired write disposition
 )
 table_ref = client.dataset(dataset_id).table(table_id)
+
 job = client.load_table_from_dataframe(data, table_ref, job_config=job_config)
 job.result()  # Wait for the job to complete
 
-print(f"Data pushed to BigQuery table: {project_id}.{dataset_id}.{table_id}")
+print(f"Data pushed to BigQuery table: {bigquery_project}.{dataset_id}.{table_id}")
 
-
-# Replace the placeholders for BigQuery project, dataset, and table IDs with your actual values. 
-# This script will load the data from PostgreSQL, transform it, and push it to the specified BigQuery table using the Google Cloud BigQuery API.
--- ssm_postgresql_to_gsheets.py
+++ ssm_postgresql_to_gsheets.py
@@ -19,16 +19,6 @@ ssm_parameters = {
     "localPortNumber": ["5432"],
 }
 
-# PostgreSQL credentials
-db_type = "PostgreSQL"
-db_host = "localhost"
-db_port = "5432"
-
-db_user = os.environ.get("DB_USER")
-db_password = os.environ.get("DB_PASSWORD")
-db_name = os.environ.get("DB_NAME")
-
-
 # Establish an SSM session
 ssm = boto3.client("ssm", region_name=region)
 ssm_start_response = ssm.start_session(
@@ -38,38 +28,7 @@ ssm_start_response = ssm.start_session(
 if ssm_start_response.get("SessionId"):
     print("SSM session successfully established.")
 
-    print([
-        db_name,
-        db_user,
-        db_password,
-        db_host,
-        ssm_parameters["localPortNumber"][0],
-    ])
-    # PostgreSQL connection
-    conn = psycopg2.connect(
-        dbname=db_name,
-        user=db_user,
-        password=db_password,
-        host=db_host,
-        port=ssm_parameters["localPortNumber"][0],
-    )
-
-    # Download data from the PostgreSQL table as a CSV
-    query = "SELECT * FROM account_transactions"
-    data = pd.read_sql_query(query, conn)
-
-    # Close the PostgreSQL connection
-    conn.close()
-
-    # Google Sheets credentials (replace with your own)
-    gspread_credentials_file = os.environ.get("GOOGLE_SHEETS_KEY_FILE")
-    spreadsheet_name = os.environ.get("GOOGLE_SHEETS_FILE_NAME")
-
-    # Upload data to Google Sheets
-    gc = gspread.service_account(filename=gspread_credentials_file)
-    spreadsheet = gc.open(spreadsheet_name)
-    worksheet = spreadsheet.get_worksheet(0)  # Adjust the worksheet index as needed
-    worksheet.update([data.columns.values.tolist()] + data.values.tolist())
+    
 
     # End the SSM session
     ssm.end_session(SessionId=ssm_start_response["SessionId"])
